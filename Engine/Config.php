aa4417a9777eee4ea6ef84d642a9f291047a7bf4
Tran Thanh An <an.tt@neo-lab.vn>
2018-05-11 11:33:02 +0700

    new edit

--- Engine/Config.php
+++ Engine/Config.php
@@ -8,7 +8,7 @@ final class Config
     // Database info (if you want to test the script, please edit the below constants with yours)
     const
     DB_HOST = 'localhost',
-    DB_NAME = 'blog',
+    DB_NAME = 'to-do-list',
     DB_USR = 'root',
     DB_PWD = 'root',
 
--- View/index.php
+++ View/index.php
@@ -1,7 +1,7 @@
 
-<?php require 'inc/header.php' ?>
-<?php require 'inc/msg.php' ?>
-<!-- <?php var_dump($this->sSuccMsg) ?> -->
+<?php require 'inc/header.php'?>
+<?php require 'inc/msg.php'?>
+<!-- <?php var_dump($this->sSuccMsg)?> -->
 <div class="w3-col l8 s12">
 <?php if (empty($this->oPosts)): ?>
     <p class="bold">There is no Blog Post.</p>
@@ -10,11 +10,11 @@
 
     <?php foreach ($this->oPosts as $oPost): ?>
         <div class="w3-card-4 w3-margin w3-white">
-        <?php if ($oPost->image) { ?>
+        <?php if (!empty($oPost->image)) {?>
           <img src="<?php echo ROOT_URL_IMAGE . $oPost->image ?>" alt="Nature" style="width:100%; height: 240px">
         <?php } else {?>
           <img src="public/upload/woods.jpg" alt="Nature" style="width:100%">
-        <?php } ?> 
+        <?php }?>
         <div class="w3-container">
           <h3><b><a href="<?=ROOT_URL?>?p=blog&amp;a=post&amp;id=<?=$oPost->id?>"><?=htmlspecialchars($oPost->title)?></a></b></h3>
           <h5>Posted on: <span class="w3-opacity"><?=$oPost->createdDate?></span></h5>
@@ -27,15 +27,15 @@
               <p><button class="w3-button w3-padding-large w3-white w3-border" onclick="window.location='<?=ROOT_URL?>?p=blog&amp;a=post&amp;id=<?=$oPost->id?>'"><b>READ MORE »</b></button></p>
             </div>
             <div class="w3-col m8 s12">
-                <?php require 'inc/control_buttons.php' ?>
+                <?php require 'inc/control_buttons.php'?>
               <p><span class="w3-padding-large w3-right"><b></b></span></p>
             </div>
           </div>
         </div>
       </div>
     <hr class="clear" />
-    <?php endforeach ?>
-  </div>  
+    <?php endforeach?>
+  </div>
     <div class="w3-col l4">
   <!-- About Card -->
   <div class="w3-card-2 w3-margin w3-margin-top">
@@ -46,24 +46,24 @@
       <p>Don't ask me, Who is she? She just a beautiful girl and she forever in my heart. There is only one happiness in this life, To LOVE and be LOVED</p>
     </div>
   </div>
-  
+
   <!-- Posts -->
   <div class="w3-card-2 w3-margin" style="max-height: 700px;overflow: scroll;">
     <div class="w3-container w3-padding">
       <h4>All Posts</h4>
     </div>
     <ul class="w3-ul w3-hoverable w3-white" id="right-content">
-      <?php foreach ($this->allPosts as $post):  ?>
+      <?php foreach ($this->allPosts as $post): ?>
       <li class="w3-padding-16">
-        <img src="<?php if ($post->image) {echo ROOT_URL_IMAGE . $post->image;} else {echo ROOT_URL_IMAGE . 'woods.jpg';} ?>" alt="Image" class="w3-left w3-margin-right" style="width: 50px;height: 50px">
+        <img src="<?php if ($post->image) {echo ROOT_URL_IMAGE . $post->image;} else {echo ROOT_URL_IMAGE . 'woods.jpg';}?>" alt="Image" class="w3-left w3-margin-right" style="width: 50px;height: 50px">
         <span class="w3-large"><a href="<?=ROOT_URL?>?p=blog&amp;a=post&amp;id=<?=$post->id?>"><?=htmlspecialchars($post->title)?></a></span><br>
         <span><?=nl2br(htmlspecialchars(mb_strimwidth($post->body, 0, 120, '...')))?></span>
       </li>
-      <?php endforeach ?>
+      <?php endforeach?>
     </ul>
   </div>
-  <hr>   
+  <hr>
 <!-- END Introduction Menu -->
 </div>
-<?php endif ?> 
-<?php require 'inc/footer.php' ?>
+<?php endif?>
+<?php require 'inc/footer.php'?>
